c363ad0c5dc3fb0d5d214a0fbb61d7bf7e47e7df
Devis <devisbl94@gmail.com>
2017-10-20 14:59:51 -0400

    Identar script

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,14 @@ const pokemonURL = identifier => `${baseURL}pokemon/${identifier}/`;
 
 const getData = (param, toGet) => {
 
-	switch (toGet) {
-		case 'pokemon':
-			param = pokemonURL(param);
-			break;
-		default:
-			// statements_def
-			break;
-	}
+    switch (toGet) {
+        case 'pokemon':
+            param = pokemonURL(param);
+            break;
+        default:
+            // statements_def
+            break;
+    }
 
     let cacheKey = param
     let cached = getFromCache(cacheKey)
@@ -26,77 +26,74 @@ const getData = (param, toGet) => {
 
             if (contentType.includes('application/json')) {
 
-				response.clone().text().then(content => {
-					sessionStorage.setItem(cacheKey, content)
-				})
+                response.clone().text().then(content => {
+                    sessionStorage.setItem(cacheKey, content)
+                })
 
                 return response.json().catch(error => {
                     return Promise.reject('Invalid JSON: ' + error.message);
                 });
 
             } else {
-            	return Promise.reject('Invalid content type: ' + contentType)
+                return Promise.reject('Invalid content type: ' + contentType)
             }
-        } else if(response.status == 400) {
-
-        	return Promise.reject('Page not found: ' + url);
-
+        } else if (response.status == 400) {
+            return Promise.reject('Page not found: ' + url);
         } else {
-        	return Promise.reject('HTTP error: ' + response.status);
+            return Promise.reject('HTTP error: ' + response.status);
         }
-
     }).catch(error => {
         return Promise.reject(error.message);
     });
 }
 
 const drawPokemon = pokemonData => {
-  const{
-    name,
-    id,
-    sprites: {front_default: image},
-    types
-  } = pokemonData;
+    const {
+        name,
+        id,
+        sprites: { front_default: image },
+        types
+    } = pokemonData;
 
-  const results = idSelector('results');
-  results.innerHTML += `
+    const results = idSelector('results');
+    results.innerHTML += `
     <span class="col-xs-4">
       <p>N° ${id}</p>
       <p>${name}</p>
       <p>${types.map( (index) => {
-      	return index.type.name;
+        return index.type.name;
       }).toLocaleString().replace(',',' - ')}</p>
       <img src="${image}" />
     </span>
   `;
 };
 
-idSelector("main-form").addEventListener( "submit", (event) => {
-	event.preventDefault();
-	let toSearch = idSelector("search-pkmn");
-	let panelResults = idSelector("panel-results");
-	let value = toSearch.value.toLowerCase();
-	toSearch.disabled = true;
-	getData(value, 'pokemon')
-		.then(data => {
-			drawPokemon(data);
-			if (panelResults.classList.contains('hidden')) {
-				panelResults.classList.remove('hidden');
-			}
-			toSearch.value = '';
-			toSearch.disabled = false;
-		})
+idSelector("main-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    let toSearch = idSelector("search-pkmn");
+    let panelResults = idSelector("panel-results");
+    let value = toSearch.value.toLowerCase();
+    toSearch.disabled = true;
+    getData(value, 'pokemon')
+        .then(data => {
+            drawPokemon(data);
+            if (panelResults.classList.contains('hidden')) {
+                panelResults.classList.remove('hidden');
+            }
+            toSearch.value = '';
+            toSearch.disabled = false;
+        })
 });
 
-function idSelector(id){
-	return document.querySelector(`#${id}`);
+function idSelector(id) {
+    return document.querySelector(`#${id}`);
 }
 
 // SIN USO
-function setInCache(id, data){
-	localStorage.setItem(id, JSON.stringify(data));
+function setInCache(id, data) {
+    localStorage.setItem(id, JSON.stringify(data));
 }
 
-function getFromCache(id){
-	return localStorage.getItem(id) ? JSON.parse(localStorage.getItem(id)) : false;
+function getFromCache(id) {
+    return localStorage.getItem(id) ? JSON.parse(localStorage.getItem(id)) : false;
 }
